0b8844dc4f53c03cdd816fc6b75cdc6bdafa2a69
Vitalii <ilovepinkpony1@gmail.com>
2019-07-25 01:44:22 +0300

    Updates

---
+++ precache-manifest.a17c841630378ef22b9733cd38e2c838.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7142bb9198dc329a04415299fe9f2f",
+    "url": "/front-main-page/index.html"
+  },
+  {
+    "revision": "4246337410c7fe00d43c",
+    "url": "/front-main-page/static/css/2.ffe8cabf.chunk.css"
+  },
+  {
+    "revision": "a0f3c9db87e8e64853cf",
+    "url": "/front-main-page/static/css/main.4e1b233b.chunk.css"
+  },
+  {
+    "revision": "4246337410c7fe00d43c",
+    "url": "/front-main-page/static/js/2.c1b4cd42.chunk.js"
+  },
+  {
+    "revision": "a0f3c9db87e8e64853cf",
+    "url": "/front-main-page/static/js/main.e6fcd4a9.chunk.js"
+  },
+  {
+    "revision": "ff2db5f6aba6ef131b3a",
+    "url": "/front-main-page/static/js/runtime~main.1a2690be.js"
+  },
+  {
+    "revision": "9c9136392fd365da6a1d40420b34d30b",
+    "url": "/front-main-page/static/media/1.9c913639.jpeg"
+  },
+  {
+    "revision": "4bc5bc0d11a44f9b507d299380518e46",
+    "url": "/front-main-page/static/media/2.4bc5bc0d.jpeg"
+  },
+  {
+    "revision": "bc18622010ac42d0fa8aac5e73fed514",
+    "url": "/front-main-page/static/media/3.bc186220.jpeg"
+  },
+  {
+    "revision": "7bdad01c1ff694b19fbfb01fdbb85963",
+    "url": "/front-main-page/static/media/Awfully-pretty-logo-web.7bdad01c.png"
+  }
+]);
